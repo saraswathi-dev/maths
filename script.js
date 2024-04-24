48fe6802aa5d028533c1706d0b0270614a929544
saraswathi-dev <saraswathichowdam.dev@gmail.com>
2024-04-24 10:55:30 +0530

    update  project

---
+++ script.js
@@ -0,0 +1,16 @@
+/*var a = prompt("enter a value");
+var b = prompt("enter b value");
+var c = prompt("enter c value");
+console.log("a value is " + a + " b value is " + b + " c value is " + c);
+a = Number(a);
+b = Number(b);
+c = Number(c);
+var sqrt_val = Math.sqrt((b * b) - (4 * a * c));
+console.log( "print of sqrt value " + sqrt_val)
+var d = (-b - sqrt_val ) / (2 * a);
+var e = (-b - sqrt_val) / (2 * a);
+
+console.log("results are " + d + " " + e);*/
+
+
+
